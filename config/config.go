ebae49a42f190d86b142a53f60278fc843c6a21d
Pratam-Kalligudda <kalliguddapratam642002@gmail.com>
2025-07-03 01:48:49 +0530

    initial setup of api gateway

---
+++ config/config.go
@@ -0,0 +1,8 @@
+package config
+
+type Config struct {
+	SECRET string
+	USER_ENDPOINT string
+	PRODUCT_ENDPOINT string
+	ORDER_ENDPOINT string
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Pratam-Kalligudda/api-gateway-go
+
+go 1.24.4
